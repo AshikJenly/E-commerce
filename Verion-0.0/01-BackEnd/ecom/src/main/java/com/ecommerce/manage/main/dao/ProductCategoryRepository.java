6431fc285bd6975d230ce5571619e3423a918aea
Ashik Jenly <jenlyashik@gmail.com>
2023-08-28 08:34:27 +0530

    Updated JPA Repo

--- Verion-0.0/01-BackEnd/ecom/src/main/java/com/ecommerce/manage/main/dao/ProductCategoryRepository.java
+++ Verion-0.0/01-BackEnd/ecom/src/main/java/com/ecommerce/manage/main/dao/ProductCategoryRepository.java
@@ -1,13 +1,21 @@
 package com.ecommerce.manage.main.dao;
 
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+import com.ecommerce.manage.main.Model.Product;
 import com.ecommerce.manage.main.Model.ProductCategory;
+
 
 
 @CrossOrigin("http://localhost:4201")
 @RepositoryRestResource(collectionResourceRel = "productCategory",path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+    List<ProductCategory> findByid(@Param("id") Long id);
+
 }
--- Verion-0.0/01-BackEnd/ecom/src/main/java/com/ecommerce/manage/main/dao/ProductRepository.java
+++ Verion-0.0/01-BackEnd/ecom/src/main/java/com/ecommerce/manage/main/dao/ProductRepository.java
@@ -1,11 +1,17 @@
 package com.ecommerce.manage.main.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.ecommerce.manage.main.Model.Product;
 
 @CrossOrigin("http://localhost:4201")
+@RepositoryRestResource(collectionResourceRel = "product",path = "products")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByid(@Param("id") Long id);
 
 }
